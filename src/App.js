39469a38c166529a8f524cb1c82c260a5b4c91d2
Leo Christen Macedo <leo.christenmacedo@gmail.com>
2022-02-01 17:09:21 -0300

    Organizando estrutura dos diretórios do projeto e iniciando o surge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React from 'react';
+import { AppContainer } from './components/AppContainer';
+import styled from 'styled-components';
+import logo from './assets/img/labeninjas-logo.png';
+import icon from './assets/img/labeninjas-Icon.png';
 
-function App() {
-	return (
-        <AppContainer />
-	)
-}
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	};
+};
 
-export default App
+export default App;
